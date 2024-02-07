83998d9256b111281389d10851b367931c8cdd0a
dadomoon <mrdadomon@gmail.com>
2024-02-06 21:08:28 -0600

    commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    
+    public static void main(String[] args) throws Exception {
+
+        String asterisco = "*";
+
+        for (asterisco.length(); asterisco.length() <= 9; asterisco+="**") {
+
+            System.out.println(asterisco);
+
+        }
+    }
+}
